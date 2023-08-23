cdeedf278cbf06ff39cc436f66d0ce8c7ea2ef45
Jeeeyoungkim <102647933+Jeeeyoungkim@users.noreply.github.com>
2023-08-23 12:22:34 +0900

    Feat : 유저  상태관리 redux 추가

---
+++ OkeyDokeyApp/src/redux/slice/userSlice.js
@@ -0,0 +1,28 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+const initialState = {
+  isLoggedIn: false,
+  access_token: null,
+  refresh_token: null,
+  nickname: null,
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      //처음 로그인
+      state.isLoggedIn = true;
+      state.access_token = action.payload.access_token;
+      state.refresh_token = action.payload.refresh_token;
+    },
+    setNickname: (state, action) => {
+      // 닉네임만 업데이트하는 액션
+      state.nickname = action.payload;
+    },
+  },
+});
+
+export const {login, setNickname} = userSlice.actions;
+export default userSlice.reducer;
