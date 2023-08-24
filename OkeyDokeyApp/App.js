24611ad163ecd972a8c7db45725ac1afa46f2a03
Jeeeyoungkim <102647933+Jeeeyoungkim@users.noreply.github.com>
2023-08-25 02:03:49 +0900

    Feat : 로그인 유지 로직 추가

--- OkeyDokeyApp/App.js
+++ OkeyDokeyApp/App.js
@@ -1,8 +1,11 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useNavigation} from '@react-navigation/native';
 
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
@@ -16,13 +19,119 @@ import Camera from './src/screens/Camera';
 
 //redux
 import {Provider} from 'react-redux';
+import {login, setNickname} from './src/redux/slice/userSlice';
 import {store} from './src/redux/store';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+
+import {createIconSetFromFontello} from 'react-native-vector-icons';
+
+const UserInfoHandler = () => {
+  const navigation = useNavigation();
+  const isLoggedIn = useSelector(state => state.user.isLoggedIn);
+  const dispatch = useDispatch();
+
+  const refreshAccessToken = async () => {
+    const body = {
+      refresh: await AsyncStorage.getItem('refresh_token'),
+    };
+
+    try {
+      const response = await axios.post(
+        'http://3.36.95.105/account/refresh/access_token/',
+        body,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+
+      const access = response.data.access;
+      const refresh = response.data.refresh;
+
+      await AsyncStorage.setItem('access_token', access);
+      await AsyncStorage.setItem('refresh_token', refresh);
+
+      console.log('success : refresh Access Token');
+    } catch (error) {
+      console.error('Error refreshing access token:', error);
+      throw error;
+    }
+  };
+
+  const fetchUserInfo = async token => {
+    try {
+      const response = await axios.get('http://3.36.95.105/account/user', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const userInfo = response.data.user;
+      dispatch(setNickname(userInfo.nickname));
+      console.log('face_registered : ', userInfo.face_registered);
+
+      if (userInfo.face_registered === false) {
+        navigation.navigate('Camera');
+      }
+
+      console.log('home으로 이동'); //userinfo 가져오는데 성공하면 home으로 이동
+      navigation.navigate('Bottom', {screen: 'Home'});
+    } catch (error) {
+      console.error('Failed to fetch user info', error);
+
+      //401 에러시 토큰 refresh
+      if (error.response && error.response.status === 401) {
+        try {
+          console.log('Attempting to refresh the access token...');
+          await refreshAccessToken();
+
+          const newAccessToken = await AsyncStorage.getItem('access_token');
+          if (newAccessToken) {
+            await fetchUserInfo(newAccessToken);
+          } else {
+            console.log('Failed to get new access token from storage.');
+          }
+        } catch (refreshError) {
+          console.error('Error refreshing access token:', refreshError);
+        }
+      }
+    }
+  };
+
+  useEffect(() => {
+    const checkToken = async () => {
+      // await AsyncStorage.removeItem('access_token');
+      // await AsyncStorage.removeItem('refresh_token');
+
+      try {
+        const accessToken = await AsyncStorage.getItem('access_token');
+        const refreshToken = await AsyncStorage.getItem('refresh_token');
+
+        if (accessToken && refreshToken) {
+          dispatch(login({accessToken, refreshToken})); //redux에 저장
+          fetchUserInfo(accessToken);
+          console.log('asyncStorage서 가져옴');
+          console.log(accessToken, '/', refreshToken);
+        } else {
+          console.log('login으로 이동');
+          navigation.navigate('Login');
+        }
+      } catch (error) {
+        console.error('Error while checking token:', error);
+      }
+    };
+
+    checkToken();
+  }, []);
+
+  return null;
+};
 
 const App = () => {
   const isLoggedIn = useSelector(state => state.user.isLoggedIn);
   const Stack = createNativeStackNavigator();
   const Tab = createBottomTabNavigator();
+
   const BottomTabScreen = () => {
     return (
       <Tab.Navigator
@@ -65,15 +174,13 @@ const App = () => {
   };
   return (
     <NavigationContainer>
+      <UserInfoHandler />
       <Stack.Navigator screenOptions={{headerShown: false}}>
-        {!isLoggedIn ? (
+        <>
+          <Stack.Screen name="Bottom" component={BottomTabScreen} />
           <Stack.Screen name="Login" component={Login} />
-        ) : (
-          <>
-            <Stack.Screen name="Bottom" component={BottomTabScreen} />
-            <Stack.Screen name="Camera" component={Camera} />
-          </>
-        )}
+          <Stack.Screen name="Camera" component={Camera} />
+        </>
       </Stack.Navigator>
     </NavigationContainer>
   );
