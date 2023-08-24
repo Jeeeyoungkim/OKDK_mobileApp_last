f703697f6775c2adff538b7e0fcab82ec652ffd0
Jeeeyoungkim <102647933+Jeeeyoungkim@users.noreply.github.com>
2023-08-25 02:03:21 +0900

    Feat : 웹뷰와 토큰 통신 로직 추가

--- OkeyDokeyApp/src/screens/Home.js
+++ OkeyDokeyApp/src/screens/Home.js
@@ -1,20 +1,47 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useRef} from 'react';
 import WebView from 'react-native-webview';
-//import CameraScreen from '../components/CameraScreen';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Home = () => {
+  const webViewRef = useRef();
+
+  const postMessage = async ({type, data}) => {
+    const accessToken = await AsyncStorage.getItem('access_token');
+    const refreshToken = await AsyncStorage.getItem('refresh_token');
+    const script = `
+    localStorage.setItem('access', ${JSON.stringify(accessToken)});
+    localStorage.setItem('refresh', ${JSON.stringify(refreshToken)});
+    console.log("localStorage 세팅완료");
+    window.ReactNativeWebView.postMessage("SetToken");`;
+
+    webViewRef.current.injectJavaScript(script);
+  };
+
+  const onWebMessage = event => {
+    const messageData = event.nativeEvent.data;
+    console.log(messageData);
+    if (messageData === 'SetToken') {
+      console.log('LocalStorage has been set in the WebView!');
+      // 앱에서 로컬스토리지에 토큰설정 후 fetchData 실행
+      webViewRef.current.injectJavaScript(`window.fetchData()`);
+    }
+  };
+
   return (
     <>
       <View style={{flex: 1}}>
         <WebView
+          ref={webViewRef}
           mixedContentMode="always"
           style={{width: '100%', height: '100%'}}
-          source={{uri: 'http://192.168.0.15:3000/'}}
+          source={{uri: 'http://192.168.0.15:3000'}}
           onError={syntheticEvent => {
             const {nativeEvent} = syntheticEvent;
             console.warn('WebView error: ', nativeEvent);
           }}
+          onLoadEnd={postMessage}
+          onMessage={onWebMessage}
         />
       </View>
     </>
