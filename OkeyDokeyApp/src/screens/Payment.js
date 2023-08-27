e0628f946e54724cc5fb9d7d86bc78ded72a8fdf
kimyeonchul <yeonchul0807@nate.com>
2023-08-27 18:36:23 +0900

    Feat: payment

--- OkeyDokeyApp/src/screens/Payment.js
+++ OkeyDokeyApp/src/screens/Payment.js
@@ -3,52 +3,19 @@ import React, {useEffect, useRef} from 'react';
 import WebView from 'react-native-webview';
 import {useNavigation} from '@react-navigation/native';
 
-const Payment = ({route}) => {
+const Payment = () => {
   const navigation = useNavigation();
-  const enroll = route.params;
-  if(enroll && enroll.enroll){
-  console.log(`payment의 enroll route 값이 잘왔니? ${enroll.enroll}`);
-}
-  const webViewRef = useRef(null);
-  useEffect(() => {
-      sendMessageToWebView();
-  }, [enroll]);
-
-  const sendMessageToWebView = () => {
-    console.log("자 웹뷰의 카메라 한테 코드전송 시작1");
-    const data = {
-      message: "웹뷰",
-      card: {
-        card_number: "9490 9488 0671 8092",
-        expiration_date: "03/24",
-        cvc_number: "585"
-      }
-    };
-    const jsonData = JSON.stringify(data);
-    console.log(webViewRef.current);
-    if (webViewRef.current) {
-      console.log("자 웹뷰의 카메라 한테 코드전송 시작2");
-      webViewRef.current.postMessage(jsonData);
-    }
-  };
   return (
     <>
       <WebView
-        source={{uri: 'http://192.168.123.103:3000/morecards'}}
-        ref={webViewRef}
-        // onLoad={sendMessageToWebView}
-        onMessage={event => {
-          console.log('받은 데이터(React) : ' + event.nativeEvent.data);
-          const parsedData = JSON.parse(event.nativeEvent.data);
-          console.log('Type:', parsedData.type);
-          console.log('Data:', parsedData.data);
-          if (parsedData && event.nativeEvent.data) {
-            navigation.navigate('CardCamera');
-          }
-          //이제 여기서 받았으니깐 나는 이동시켜주면 되
+        mixedContentMode="always"
+        style={{width: '100%', height: '100%'}}
+        source={{uri: 'http://192.168.201.12:3000/payment'}}
+        onError={syntheticEvent => {
+          const {nativeEvent} = syntheticEvent;
+          console.warn('WebView error: ', nativeEvent);
         }}
       />
-      {/* <CameraScreen /> */}
     </>
   );
 };
