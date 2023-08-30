dd19ea474ff58078d06d5aa2cb8eeae2b8fcd53d
Jeeeyoungkim <102647933+Jeeeyoungkim@users.noreply.github.com>
2023-08-31 01:57:11 +0900

    Feat : 최초 얼굴 등록시 뒤로가기 막기

--- OkeyDokeyApp/src/screens/Camera.js
+++ OkeyDokeyApp/src/screens/Camera.js
@@ -1,11 +1,28 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {View, Text, BackHandler} from 'react-native';
+import React, {useEffect} from 'react';
 import CameraScreen from '../components/CameraScreen';
-import {useNavigation, useRoute} from '@react-navigation/native';
+import {useRoute} from '@react-navigation/native';
 
 const Camera = () => {
   const route = useRoute();
   const updateState = route.params ? route.params : false;
+
+  useEffect(() => {
+    const handleBackPress = () => {
+      if (!updateState) {
+        return true;
+      } else {
+        return false;
+      }
+    };
+
+    BackHandler.addEventListener('hardwareBackPress', handleBackPress);
+
+    return () => {
+      BackHandler.removeEventListener('hardwareBackPress', handleBackPress);
+    };
+  }, []);
+
   return (
     <View style={{flex: 1}}>
       <CameraScreen updateState={updateState} />
